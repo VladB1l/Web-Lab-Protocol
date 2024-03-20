594895dcf8180001eff48e6fbab5d5b686870293
Vladyslav Bilokon <vlad2004bil@gmail.com>
2024-03-20 12:33:07 +0200

    Edited links

--- scipt.js
+++ scipt.js
@@ -2,7 +2,7 @@ const nav = document.querySelector("nav");
 const iframe = document.getElementById("iframe")
 let navList = [];
 
-for (let i = 1; i <= 8; i++) {
+for (let i = 1; i <= 9; i++) {
     navList.push(`Лабораторна робота №${i}`);
 }
 
